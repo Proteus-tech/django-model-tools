4338d8897e48b14fd21cc7baee7661832be1e7d9
Pam Suebvisai <sinapam@proteus-tech.com>
2012-04-17 15:59:07 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='django-model-tools',
+    version='0.1',
+    description='Utility functions or decorators to be used with Django model classes',
+    author='Proteus Technologies Co. Ltd.',
+    author_email='team@proteus-tech.com',
+    url='https://github.com/Proteus-tech/django-model-tools',
+    requires=['django'], # should be able to support all django version
+    packages=['django_model_tools'])
