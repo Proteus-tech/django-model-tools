0f2ff868c646329e5e62b0e8f2114483c17ddf82
Pam Suebvisai <sinapam@proteus-tech.com>
2012-04-17 16:36:09 +0700

    fixed sample_app/models.py to force more than 1 field to not be blank changed version to 0.2

--- sample_app/models.py
+++ sample_app/models.py
@@ -5,7 +5,8 @@ from django_model_tools.decorators import force_save_not_null_nor_blank
 # Create your models here.
 class SampleModel(models.Model):
     a_field = models.CharField(max_length=5) # this is by default blank=True, null= True
+    b_field = models.CharField(max_length=5) # this is by default blank=True, null= True
 
-    @force_save_not_null_nor_blank('a_field')
+    @force_save_not_null_nor_blank('a_field', 'b_field')
     def save(self, force_insert=False, force_update=False, using=None):
         super(SampleModel, self).save(force_insert, force_update, using)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-model-tools',
-    version='0.1',
+    version='0.2',
     description='Utility functions or decorators to be used with Django model classes',
     author='Proteus Technologies Co. Ltd.',
     author_email='team@proteus-tech.com',
